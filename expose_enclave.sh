444c3dbbb36732d5288c7cba4732552e621f37a2
rek3000 <torek3k@gmail.com>
2025-11-13 10:45:44 +0700

    update server

--- expose_enclave.sh
+++ expose_enclave.sh
@@ -11,6 +11,7 @@ sleep 5
 # Secrets-block
 # No secrets: create empty secrets.json for compatibility
 echo '{}' > secrets.json
+# No secrets: create empty secrets.json for compatibility
 # This section will be populated by configure_enclave.sh based on secret configuration
 
 cat secrets.json | socat - VSOCK-CONNECT:$ENCLAVE_CID:7777
